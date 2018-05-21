b397df2da74ad126627a2e561ba24406a92e05a3
Wesley Yao <wesyao@microsoft.com>
2018-05-21 11:21:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sample-teams-bot
---
+++ index.js
@@ -0,0 +1,25 @@
+let restify = require('restify');
+let server = restify.createServer();
+
+var builder = require("botbuilder");
+var teams = require("botbuilder-teams");
+
+
+let connector = new teams.TeamsChatConnector({
+    appId: '', // bot id
+    appPassword: '', // bot secret
+});
+
+// console.log(connector);
+
+server.get('/echo/:name', (req, res, next) => {
+    res.send(req.params);
+    return next();
+});
+
+server.post('/api/v1/bot/messages', connector.listen());
+let bot = new builder.UniversalBot(connector);
+
+bot.on("conversationUpdate", (message) => console.log(message));
+
+server.listen(8080, () => console.log('Listening on port 8080'));
